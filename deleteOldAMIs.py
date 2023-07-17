3ead37e73ad0291a25c324fa6eb7e4f04dae6eec
Sampath Hewage <samdhew@users.noreply.github.com>
2023-07-17 22:32:49 +0530

    Update deleteOldAMIs.py

--- deleteOldAMIs.py
+++ deleteOldAMIs.py
@@ -7,7 +7,7 @@ import re
 from datetime import datetime, timedelta
 import boto3
 
-# ACCOUNT_ID = boto3.client('sts').get_caller_identity().get('Account')
+ACCOUNT_ID = boto3.client('sts').get_caller_identity().get('Account')
 fileNameAMI = "deleteAMIs_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".txt"
 fileNameSnapshot = "deleteSnapshots_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".txt"
 
@@ -51,21 +51,21 @@ def main():
         file = open(fileNameAMI, 'a+')
         # print('Deregistering {} ({})'.format(image.name, image.id))
         file.write('Deregistering {} ({})\n'.format(image.name, image.id))
-        # image.deregister()
+        image.deregister()
         file.close()
 
     # Delete unattached snapshots
-    # print('Deleting snapshots.')
-    # images = [image.id for image in ec2.images.all()]
-    # for snapshot in ec2.snapshots.filter(OwnerIds=[ACCOUNT_ID]):
-    #     print('Checking {}'.format(snapshot.id))
-    #     r = re.match(r".*for (ami-.*) from.*", snapshot.description)
-    #     if r:
-    #         if r.groups()[0] not in images:
-    #             file = open(fileNameSnapshot, 'a+')
-    #             print('Deleting {}'.format(snapshot.id))
-    #             # snapshot.delete()
-    #             file.close()
+    print('Deleting snapshots.')
+    images = [image.id for image in ec2.images.all()]
+    for snapshot in ec2.snapshots.filter(OwnerIds=[ACCOUNT_ID]):
+        print('Checking {}'.format(snapshot.id))
+        r = re.match(r".*for (ami-.*) from.*", snapshot.description)
+        if r:
+            if r.groups()[0] not in images:
+                file = open(fileNameSnapshot, 'a+')
+                print('Deleting {}'.format(snapshot.id))
+                snapshot.delete()
+                file.close()
     
 if __name__ == "__main__":
     main()
